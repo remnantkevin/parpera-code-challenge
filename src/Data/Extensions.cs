93326e48b599b70281652dda7dbe2f00338cd69e
Kevin Elliott <skiimilk@gmail.com>
2022-01-21 01:18:07 +0800

    Remove unneeded braces in Extensions

--- src/Data/Extensions.cs
+++ src/Data/Extensions.cs
@@ -4,15 +4,13 @@ public static class Extensions
 {
     public static void SeedDatabase(this IHost host)
     {
+        using (var scope = host.Services.CreateScope())
         {
-            using (var scope = host.Services.CreateScope())
+            var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<TransactionContext>();
+            if (context.Database.EnsureCreated())
             {
-                var services = scope.ServiceProvider;
-                var context = services.GetRequiredService<TransactionContext>();
-                if (context.Database.EnsureCreated())
-                {
-                    DatabaseInitializer.Initialize(context);
-                }
+                DatabaseInitializer.Initialize(context);
             }
         }
     }
